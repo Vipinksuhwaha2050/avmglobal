a778158be92c3cbb75f6109267c798e1432a4877
vip <kushji5@gmail.com>
2023-01-24 15:07:30 +0530

    34

--- avmglobal/static/js/sidebar.js
+++ avmglobal/static/js/sidebar.js
@@ -1,18 +1,71 @@
-function overlay(isShow){
-    var elm = document.getElementById('overlay')
-    if (isShow) {
-        elm.style.display = 'block';
-    } else {
-        elm.style.display = 'none';
-    }
-    }
+
+// Add an event listener to the toggle button
+
+document.getElementById("toggleSidebarBtn").addEventListener("click", function() {
     
-    function openNav() {
-    overlay(true);
-        document.getElementById('mySidenav').style.width="250px";
-    }
+    // Toggle the "open" class on the sidebar element
     
-    function closeNav() {
-        overlay(false);
-        document.getElementById('mySidenav').style.width="0"
-    }
+        document.getElementById("sidebar").classList.toggle("open");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function toggleSideBar() {
+//     var myDiv = document.getElementById("sideBarDiv");
+//     if (myDiv.style.display === "none") {
+//         myDiv.style.display = "block";
+//     } else {
+//         myDiv.style.display = "none";
+//     }
+//     }
+
+
+
+    
+
+// function overlay(isShow){
+//     if (isShow) {
+//         document.getElementById('overlay').style.display = 'block';
+//     } else {
+//         document.getElementById('overlay').style.display = 'none';
+//     }
+//     }
+    
+
+    
+//     function openNav(){
+//     overlay(true);
+//         document.getElementById('mySidenav').style.width="250px";
+//     }
+    
+//     function closeNav() {
+//         overlay(false);
+//         document.getElementById('mySidenav').style.width="80px";
+//     }
